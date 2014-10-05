cbb54f7194564558de214105b47cf7327d42f848
unknown <vr222bt@student.lnu.se>
2014-10-05 20:17:27 +0200

    Added methods and constructor

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -13,5 +13,20 @@ namespace _1DV402.S2.L1A
         private int _number;
         public const int MaxNumberOfGuesses = 7;  
 
+
+        public void Initialize()
+        {
+
+        }
+
+        public bool MakeGuess(int number)
+        {
+            return false;
+        }
+
+        SecretNumber()
+        {
+            Initialize();
+        }
     }
 }
