f2f2322040ab9596162024c2312910a8e883a5c4
unknown <vr222bt@student.lnu.se>
2014-10-05 20:55:39 +0200

    Started working on MakeGuess, added counter and elsif

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -8,7 +8,7 @@ namespace _1DV402.S2.L1A
 {
     public class SecretNumber
     {
-
+        //Deklarerar fält
         private int _count;
         private int _number;
         public const int MaxNumberOfGuesses = 7;  
@@ -16,17 +16,40 @@ namespace _1DV402.S2.L1A
 
         public void Initialize()
         {
+            //Ger _count ett startvärde
             _count = 0;
+            //Tar fram det hemliga talet
             Random random = new Random();
             _number = random.Next(1, 101);
         }
 
         public bool MakeGuess(int number)
         {
-            return false;
+            //Håller ordning på antalet gissningar som gjorts
+            _count++;
+
+            //Undersöker om gissningen är rätt
+            if (_number == number)
+            {
+                Console.WriteLine("");
+                return true;
+            }
+            //Undersöker om det hemliga talet är högre än gissningen
+            else if (_number > number)
+            {
+                Console.WriteLine("");
+                return false;
+            }
+            //Om det hemliga talet är lägre än gissningen
+            else
+            {
+                Console.WriteLine("");
+                return false;
+            }
+            
         }
 
-        SecretNumber()
+        public SecretNumber()
         {
             Initialize();
         }
